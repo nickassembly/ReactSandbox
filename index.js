5d27d81d26eb1c5d7cd5665ecf64944f808608aa
nickguerra <nickg@maliciousgames.com>
2022-07-21 06:39:30 -0500

    refactored page into function

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 
-const page = (
+function TemporaryName() {
+  return (
     <div>
-       <h1>React Website</h1>
-       <h3>Use React for many reasons</h3>
-       <ol>
-         <li>Composable</li>
-         <li>Declarative</li>
-         <li>Actively Maintained</li>
-       </ol>
-    </div>
-);
+    <h1>React Website</h1>
+    <h3>Use React for many reasons</h3>
+    <ol>
+      <li>Composable</li>
+      <li>Declarative</li>
+      <li>Actively Maintained</li>
+    </ol>
+ </div>
+  );
+}
 
-ReactDOM.render(page, document.getElementById("root"));
+ReactDOM.render(<TemporaryName />, document.getElementById("root"));
 
