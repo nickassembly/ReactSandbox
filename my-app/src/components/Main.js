ea7efea06022e96e98db41d47f4bafa799627003
nickguerra <nickg@maliciousgames.com>
2022-07-24 06:44:42 -0500

    Added fonts and basic project content

---
+++ my-app/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Main () {
+    return (
+     <div>
+       <h1 className="font-link">Fun facts about React</h1>
+
+       <ul>
+          <li className="font-link">Was original created by some genius</li>
+          <li className="font-link">It is maintained by Facebook</li>
+          <li className="font-link">Is Composable</li>
+       </ul>
+
+     </div>
+    );
+}
