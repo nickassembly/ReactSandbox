95e5380352573ea96f7597ab3ec85d5a6451723c
nickguerra <nickg@maliciousgames.com>
2022-07-26 06:34:20 -0500

    Added bnb clone stub

---
+++ air-bnb-experience/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+     <Navbar />
+    </div>
+  );
+}
---
+++ air-bnb-experience/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import bnbLogo from "../images/airbnb-logo.jpg";
+
+export default function Navbar() {
+    return (
+     <nav>
+        <img src={bnbLogo} />
+     </nav>
+    );
+}
