1171f1ed25821d4e14869bc397d76fc68c195fa0
nickguerra <nickg@maliciousgames.com>
2022-08-02 06:37:37 -0500

    practice with map methods

--- air-bnb-experience/src/App.js
+++ air-bnb-experience/src/App.js
@@ -9,7 +9,15 @@ export default function App() {
     <div className="App">
      <Navbar />
      <Hero />
-     <Card /> 
+     <Card 
+       img = "./images/NoImageAvailable.png"
+       rating = "5.0"
+       reviewCount = {6}
+       country = "kellerville"
+       title = "Life lessons with Keller ZeCar"
+       price = {136}
+     /> 
+     
     </div>
   );
 }
--- air-bnb-experience/src/index.js
+++ air-bnb-experience/src/index.js
@@ -11,6 +11,29 @@ root.render(
   </React.StrictMode>
 );
 
+//////////////////////////////////////////////////// Notes on Map
+console.log("**Map Method**");
+
+const nums = [1,2,3,4,5];
+const squared = nums.map(function(item) {
+    return item * item
+});
+
+const names = ["alice", "bob", "charlie", "danielle"]
+const uppercase = names.map((name) => {
+  return name[0].toUpperCase() + name.slice(1);
+});
+
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"];
+const elements = pokemon.map(mon => {
+return `<p>${mon}</p>`
+});
+
+// console.log(squared);
+// console.log(uppercase);
+// console.log(elements);
+///////////////////////////////////////////////////////////////////
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
