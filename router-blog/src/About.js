272d42ef1b0e605d006daf70d829751a51c0d215
nickguerra <nickg@maliciousgames.com>
2022-09-14 05:03:52 -0500

    first commit

---
+++ router-blog/src/About.js
@@ -0,0 +1,12 @@
+
+const About = () => {
+    return (
+      <main className='About'>
+        <h2>About</h2>
+        <p style={{ marginTop: "1rem"}}>This is a React blog using router</p>
+      </main>
+    )
+  }
+  
+  export default About
+  
---
+++ router-blog/src/context/DataContext.js
@@ -0,0 +1,8 @@
+import { createContext, useState, useEffect } from 'react';
+
+const DataContext = createContext({});
+
+export const DataProvider = ({ children }) => {
+    // Use Context Hook tutorial: Chapter 21 (7:18 in)
+    // https://www.youtube.com/watch?v=RVFAyFWO4go&list=WL&index=5&t=24066s
+}
