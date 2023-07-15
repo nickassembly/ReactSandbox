58e4c93ce3e61a3d20cc97d289637ce752fa84a5
nickguerra <nickg@maliciousgames.com>
2023-07-15 05:46:40 -0500

    updated counters to add logic in jsx

--- understanding-react/app.js
+++ understanding-react/app.js
@@ -1,26 +1,45 @@
 const rootNode = document.getElementById('app');
 const root = ReactDOM.createRoot(rootNode);
+let counterName = "One";
 root.render(React.createElement(App));
 
 function App() {
+    const counterOne = <Counter name={counterName} />;
+    const counterTwo = <Counter2 name={counterName} />;
     return (
-        <section>
+        <>
             <h1> Counters </h1>
             <section>
-                <Counter />
+                {counterName === "One" ? counterOne : counterTwo}
             </section>
-        </section>
+        </>
     );
 }
 
-function Counter() {
+function Counter({ name }) {
     return (
         <article>
-            <h2> Counter </h2>
+            <h2> Counter {name}</h2>
             <p> You clicked 1 times </p>
-            <button className="button">Click ne</button>
+            <button className="button">Click Me</button>
         </article>
     );
+}
+
+function Counter2({ name }) {
+    return (
+        <article>
+            <h2> Counter {name}</h2>
+            <p> Times clicked: 1 </p>
+            <button className="button">Click Me</button>
+        </article>
+    );
+}
+
+function rerender() {
+    console.log("Renender...");
+    counterName = "Two";
+    root.render(React.createElement(App));
 }
 
 
