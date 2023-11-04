c113debb4191c8fdb9e9e5e22d2f767ae88cb650
nickguerra <nickg@maliciousgames.com>
2023-11-04 05:32:05 -0500

    added custom hooks

--- understanding-react/app.js
+++ understanding-react/app.js
@@ -1,43 +1,65 @@
 
-// temp notes
 const rootNode = document.getElementById('app');
 const root = ReactDOM.createRoot(rootNode);
 root.render(<App />);
 
 function App() {
-    const ref = React.useRef();
-    React.useEffect(() => {
-        ref.current.focus();
-    }, []);
 
     return (
         <>
             <h1>Counters</h1>
             <section>
                 <Counter name="One" />
-                <Counter name="Two" ref={ref} />
+                <Counter name="Two" />
             </section>
         </>
     );
 }
 
-const Counter = React.forwardRef(function Counter(props, buttonRef) {
+function useDocumentTitle(title) {
+    return React.useEffect(() => {
+        const originalTitle = document.title;
+        document.title = title;
 
-    const [numOfClicks, setNumOfClicks] = React.useState({ total: 0 });
+        return () => {
+            document.title = originalTitle;
+        }
+    }, [title])
+}
+
+function useCounter() {
+    const [counterVal, setCounterVal] = React.useState({ total: 0 });
+
+    const increment = () => {
+        setCounterVal({ ...counterVal, total: counterVal.total + 1 });
+    }
+
+    return [
+        counterVal,
+        increment
+    ]
+
+}
+
+function Counter(props) {
+
+    const [counter, incrementCounter] = useCounter();
+
+    const updateTitle = useDocumentTitle("Clicks: " + counter.total);
 
     function handleClick() {
-        setNumOfClicks({ ...numOfClicks, total: numOfClicks.total + 1 });
+        incrementCounter();
     }
 
     return (
         <article>
             <h2>Counter {props.name}</h2>
-            <p>You clicked {numOfClicks.total} times</p>
+            <p>You clicked {counter.total} times</p>
             <p>
-                <button className="button" onClick={handleClick} ref={buttonRef}>
+                <button className="button" onClick={handleClick} >
                     Click me
                 </button>
             </p>
         </article>
     );
-});
+}
