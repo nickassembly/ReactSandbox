3f0b5be057991c3b62ffc4bedf8703b7baad1999
nickguerra <nickg@maliciousgames.com>
2023-11-04 04:44:03 -0500

    updated course work

--- understanding-react/app.js
+++ understanding-react/app.js
@@ -1,57 +1,41 @@
 const rootNode = document.getElementById('app');
-
-// Hooks -- JS objects attached to (other JS objects) nodes of the fiber tree
-
 const root = ReactDOM.createRoot(rootNode);
-let counterName = "One";
 root.render(<App />);
-console.dir(rootNode);
 
 function App() {
+    const ref = React.useRef();
+    React.useEffect(() => {
+        ref.current.focus();
+    }, []);
+
     return (
         <>
             <h1>Counters</h1>
             <section>
                 <Counter name="One" />
+                <Counter name="Two" ref={ref} />
             </section>
         </>
     );
 }
 
-function Counter(props) {
+const Counter = React.forwardRef(function Counter(props, buttonRef) {
 
     const [numOfClicks, setNumOfClicks] = React.useState({ total: 0 });
-
-    React.useEffect(() => {
-        const message = `Number of clicks is: ${numOfClicks.total}`;
-        const id = setInterval(() => {
-            console.log(message);
-        }, 2000);
-
-        return () => {
-            clearInterval(id);
-        }
-    }, [numOfClicks.total]);
 
     function handleClick() {
         setNumOfClicks({ ...numOfClicks, total: numOfClicks.total + 1 });
     }
 
-
     return (
         <article>
-            <h2> Counter {props.name}</h2>
-            <p> You clicked {numOfClicks.total} times </p>
+            <h2>Counter {props.name}</h2>
+            <p>You clicked {numOfClicks.total} times</p>
             <p>
-                <button className="button" onClick={handleClick}>Click Me</button>
+                <button className="button" onClick={handleClick} ref={buttonRef}>
+                    Click me
+                </button>
             </p>
         </article>
     );
-}
-
-
-
-
-
-
-
+});
